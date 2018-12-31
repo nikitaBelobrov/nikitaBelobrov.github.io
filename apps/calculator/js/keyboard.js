d6c4853cb0010f36abf7fb9e76d6d7cb4a7a60c1
Nikita Belobrov <nik.belobrov2018@gmail.com>
2018-12-31 12:51:36 +0300

    Переместил код в mouse.js

--- apps/calculator/js/keyboard.js
+++ apps/calculator/js/keyboard.js
@@ -1,13 +1,3 @@
 /*jshint esversion: 6*/
 /*globals $, chrome, document, console, SKUF, setTimeout, console, window, log*/
 "use strict";
-
-const screen = document.querySelector('.screen');
-const numbers = document.querySelectorAll('.numbers > button');
-const operators = document.querySelectorAll('.operators > button');
-
-numbers.forEach(numBtn => {
-  numBtn.onclick = function() {
-    screen.value += this.textContent;
-  };
-});
