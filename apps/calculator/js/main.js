ec05e354af8438ba7c3391a07db3188044f79dba
Nikita Belobrov <nik.belobrov2018@gmail.com>
2018-12-31 12:55:32 +0300

    Создал файл main.js для глобальных функций и переменных

---
+++ apps/calculator/js/main.js
@@ -0,0 +1,5 @@
+/*jshint esversion: 6*/
+/*globals $, chrome, document, console, SKUF, setTimeout, console, window, log*/
+"use strict";
+
+const screen = document.querySelector('.screen');
--- apps/calculator/js/mouse.js
+++ apps/calculator/js/mouse.js
@@ -2,7 +2,6 @@
 /*globals $, chrome, document, console, SKUF, setTimeout, console, window, log*/
 "use strict";
 
-const screen = document.querySelector('.screen');
 const numbers = document.querySelectorAll('.numbers > button');
 const operators = document.querySelectorAll('.operators > button');
 
