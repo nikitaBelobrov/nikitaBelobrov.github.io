870832874937900e2655c8b35c186910c7b51784
Nikita Belobrov <nik.belobrov2018@gmail.com>
2018-12-31 11:24:49 +0300

    Добавил ссылки на экран и кнопки

--- apps/calculator/js/keyboard.js
+++ apps/calculator/js/keyboard.js
@@ -1,3 +1,8 @@
 /*jshint esversion: 6*/
 /*globals $, chrome, document, console, SKUF, setTimeout, console, window, log*/
 "use strict";
+
+const screen = document.querySelector('.screen');
+const numbers = document.querySelectorAll('.numbers > button');
+const operators = document.querySelectorAll('.operators > button');
+
